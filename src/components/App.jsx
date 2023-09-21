7809c40c628a79ae4f09a7a8ce9e350cc4a2f381
maksymnl <adw0988245131@gmail.com>
2023-09-21 15:45:15 +0300

    для ментора

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,29 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: ' ',
   };
+
+  // const initialValue = {
+  //   contacts: [
+  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ],
+  //   name: '',
+  //   number: '',
+  //   filter: '',
+  // };
+
+  // export class App extends Component {
+  //   state = { ...initialValue };
 
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
@@ -50,6 +70,7 @@ export class App extends Component {
           contacts={this.state.contacts}
           onDelete={this.deletContact}
         />
+
         <Filter value={filter} onChange={this.chanchFilter} />
       </div>
     );
